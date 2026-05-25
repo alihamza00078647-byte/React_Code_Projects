da546a091ff927cf152a6b08e0178146dd8442fe
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-05-25 17:02:49 +0500

    React Code

---
+++ Lecture 3 Install and Use bootstrap/vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+function App(){
+  return <div>
+    <h1>Hello World</h1>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
+  </div>
+}
+
+
+export default App;
