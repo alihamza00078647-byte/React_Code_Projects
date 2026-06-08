8540dde214201c4c707257c3bf3322ecf409d98f
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-06-08 16:38:47 +0500

    React Code

---
+++ Lecture 20 useEffect/vite-project/src/components/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const LoadingSpinner = () => {
+    return (
+        <div className="spinner-border" role="status" style={{marginTop : "40px"}}>
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    )
+}
+
+
+export default LoadingSpinner;
